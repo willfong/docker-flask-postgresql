62d937a84b769ba86d55c545e82715fbabed7e52
Will Fong <willfong@pm.me>
2020-01-28 22:58:55 +0800

    google auth

--- app/auth.py
+++ app/auth.py
@@ -5,32 +5,14 @@ import pprint
 from datetime import datetime, timedelta
 from functools import wraps
 from flask import Blueprint, redirect, url_for, flash, session, render_template, request, current_app, jsonify
-from flask_dance.contrib.github import make_github_blueprint, github
-from flask_dance.contrib.google import make_google_blueprint, google
-from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
+
 from . import db
 
 blueprint = Blueprint("auth", __name__, url_prefix="/auth")
 
 FACEBOOK_URL_APP_TOKEN = f'https://graph.facebook.com/oauth/access_token?client_id={os.environ.get("FACEBOOK_CLIENT_ID")}&client_secret={os.environ.get("FACEBOOK_CLIENT_SECRET")}&grant_type=client_credentials'
 
-login_github = make_github_blueprint(
-    client_id=os.environ.get("GITHUB_CLIENT_ID"),
-    client_secret=os.environ.get("GITHUB_CLIENT_SECRET"),
-    redirect_to="auth.oauth_github"
-)
 
-login_google = make_google_blueprint(
-    client_id=os.environ.get("GOOGLE_CLIENT_ID"),
-    client_secret=os.environ.get("GOOGLE_CLIENT_SECRET"),
-    redirect_to="auth.oauth_google"
-)
-
-login_facebook = make_facebook_blueprint(
-    client_id=os.environ.get("FACEBOOK_CLIENT_ID"),
-    client_secret=os.environ.get("FACEBOOK_CLIENT_SECRET"),
-    redirect_to="auth.oauth_facebook"
-)
 
 # TODO: Don't know why my decorator doesn't work.
 def login_required(func):
@@ -59,12 +41,29 @@ def facebook_find_or_create_user(id):
     query = "INSERT INTO users (facebook_id, last_login) VALUES (%s, NOW()) ON CONFLICT (facebook_id) DO UPDATE SET last_login = NOW() RETURNING id"
     params = (id,)
     user_id = db.write(query, params, returning=True)[0]
-    return user_id    
+    return user_id  
+
+def google_verify_access_token(access_token):
+    # We're doing it the lazy way here. What we get from the client side is JWT, we can just verify that instead of calling Google
+    # Reason for that is to reduce the amount of dependencies for this, a demo app
+    # For production, we should do it the right way by using google-auth 
+
+    jwt = requests.get(f'https://oauth2.googleapis.com/tokeninfo?id_token={access_token}').json()
+    if jwt.get('error'):
+        print(jwt.get('error_description'))
+        return jwt.get('error_description')
+    return jwt['sub']
+
+def google_find_or_create_user(id):
+    query = "INSERT INTO users (google_id, last_login) VALUES (%s, NOW()) ON CONFLICT (google_id) DO UPDATE SET last_login = NOW() RETURNING id"
+    params = (id,)
+    user_id = db.write(query, params, returning=True)[0]
+    return user_id 
 
 def token_required(f):
     @wraps(f)
     def _verify(*args, **kwargs):
-        auth_token = request.headers.get('Authorization', '')
+    auth_token = request.headers.get('Authorization', '')
 
         invalid_msg = {
             'message': 'Invalid token. Registeration and / or authentication required',
@@ -86,8 +85,9 @@ def token_required(f):
     return _verify
 
 @blueprint.route("/login/facebook", methods=['POST'])
-def login():
+def facebook_login():
     access_token = request.json.get("accessToken")
+    # TODO: Handle bad access token
     facebook_id = facebook_verify_access_token(access_token)
     user_id = facebook_find_or_create_user(facebook_id)
     token = jwt.encode({
@@ -98,6 +98,52 @@ def login():
         current_app.config['SECRET_KEY'])
     return jsonify({ 'token': token.decode('UTF-8') })
 
+@blueprint.route("/login/google", methods=['POST'])
+def google_login():
+    access_token = request.json.get("accessToken")
+    google_id = google_verify_access_token(access_token)
+    user_id = google_find_or_create_user(google_id)
+    token = jwt.encode({
+        'user_id': user_id,
+        'google_id': google_id,
+        'iat': datetime.utcnow(),
+        'exp': datetime.utcnow() + timedelta(minutes=30)},
+        current_app.config['SECRET_KEY'])
+    return jsonify({ 'token': token.decode('UTF-8') })
+
+
+@blueprint.route("/logout")
+def logout():
+    session.clear()
+    flash("Successfully logged out!", "success")
+    return redirect(url_for("index.home"))
+
+
+
+
+
+'''
+from flask_dance.contrib.github import make_github_blueprint, github
+from flask_dance.contrib.google import make_google_blueprint, google
+from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
+
+login_github = make_github_blueprint(
+    client_id=os.environ.get("GITHUB_CLIENT_ID"),
+    client_secret=os.environ.get("GITHUB_CLIENT_SECRET"),
+    redirect_to="auth.oauth_github"
+)
+
+login_google = make_google_blueprint(
+    client_id=os.environ.get("GOOGLE_CLIENT_ID"),
+    client_secret=os.environ.get("GOOGLE_CLIENT_SECRET"),
+    redirect_to="auth.oauth_google"
+)
+
+login_facebook = make_facebook_blueprint(
+    client_id=os.environ.get("FACEBOOK_CLIENT_ID"),
+    client_secret=os.environ.get("FACEBOOK_CLIENT_SECRET"),
+    redirect_to="auth.oauth_facebook"
+)
 
 @blueprint.route("/oauth/github")
 def oauth_github():
@@ -154,11 +200,4 @@ def oauth_facebook():
     session["user_id"], session["user_name"] = db.write(query, params, returning=True)
     flash("Successfully logged in via Facebook!", "success")
     return redirect(url_for("app.home"))
-
-
-@blueprint.route("/logout")
-def logout():
-    session.clear()
-    flash("Successfully logged out!", "success")
-    return redirect(url_for("index.home"))
-
+'''
